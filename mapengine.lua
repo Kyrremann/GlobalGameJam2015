752b7b47128652cb543e55a95acdaa14c0af6fb5
Kyrre Havik Eriksen <kyrremann@gmail.com>
2015-01-24 21:05:05 +0100

    Forgot to add .self and table count to the check

--- mapengine.lua
+++ mapengine.lua
@@ -81,7 +81,7 @@ function Engine:removeShapes()
 end
 
 function Engine:start(level)
-   if shapes then
+   if self.shapes and #self.shapes > 0 then
       self:removeShapes()
    end
    self.shapes = {}
