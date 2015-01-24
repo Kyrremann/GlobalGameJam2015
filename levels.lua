fc8ab7695e203b8e231dd8a3a8832daa0ea2a302
Kyrre Havik Eriksen <kyrremann@gmail.com>
2015-01-24 22:13:20 +0100

    Started working on adding new levels

--- levels.lua
+++ levels.lua
@@ -1,4 +1,15 @@
-return { 
+return {
+   {
+      title = "What to do now?",
+      goal = "Record giraffe movement with the arrows.\n'Right' start the record, release to start the movements!",
+      map = {
+	 {0,0,0,0,0,0,0,0,0,0},
+	 {0,0,0,0,0,0,0,0,0,0},
+	 {0,0,0,0,0,0,0,0,0,0},
+	 {0,0,0,0,0,0,0,0,0,0},
+	 {1,1,1,1,1,1,1,1,1,1}
+      }
+   },
    {
       title = "What to do now?",
       goal = "Can you manage to run?",
